42733bf5bebb14753b6f9e3b4f6d5d02b9d25cbb
soohyun <soohyun_kim2@tmax.co.kr>
2023-02-24 16:58:45 +0900

    feat: project init (cra/react@17/yarn-berry/eslint/prettier)

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true, // 따옴표 고정
+  semi: true, // 코드 마지막에 세미콜론 사용
+  useTabs: false, // 탭의 사용을 금하고 스페이스바 사용으로 대체
+  tabWidth: 2, // 탭 크기
+  trailingComma: 'all', // 객체나 배열 키:값 뒤에 항상 Comma(,) 추가
+  printWidth: 120, // 줄 당 max length
+  arrowParens: 'avoid', // 화살표 함수가 하나의 매개변수를 받을 때 괄호를 생략
+};
